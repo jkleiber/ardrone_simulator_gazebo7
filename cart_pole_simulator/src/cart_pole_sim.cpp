38c38d8b911bc8dc3fe6eefe048da78e414a06b3
Justin Kleiber <jkleiber8@gmail.com>
2020-05-07 15:55:29 -0500

    Adds nonlinear dynamics stepper to the simulation

--- cart_pole_simulator/src/cart_pole_sim.cpp
+++ cart_pole_simulator/src/cart_pole_sim.cpp
@@ -1,8 +1,11 @@
 
 #include <ros/ros.h>
 #include <eigen3/Eigen/Dense>
+#include <boost/numeric/odeint.hpp>
+#include <boost/numeric/odeint/algebra/vector_space_algebra.hpp>
 
-#include <std_msgs/Float64.h>
+#include <std_msgs/Empty.h>
+#include <gtddp_drone_msgs/ctrl_data.h>
 #include <gtddp_drone_msgs/state_data.h>
 
 // Constants
@@ -10,9 +13,7 @@ const double gravity = 9.81;
 
 // Initialization flags
 bool sim_init = false;
-
-// Track the force
-double force;
+bool sim_started = false;
 
 // System parameters
 double bob_mass;
@@ -20,18 +21,81 @@ double cart_mass;
 double max_force;
 double pole_length;
 
-// Track state
+// Track state and control
 Eigen::Vector4d state;
+Eigen::VectorXd _u = Eigen::VectorXd::Zero(1);
 
+// ODE solver
+boost::numeric::odeint::runge_kutta_dopri5<Eigen::VectorXd, double, Eigen::VectorXd, double, boost::numeric::odeint::vector_space_algebra> stepper;
 
 // System state Publisher
 ros::Publisher state_pub;
 
+// Time tracking
+ros::Time prev_time;
 
-void controlCallback(const std_msgs::Float64::ConstPtr& ctrl)
+
+
+void dynamics(const Eigen::Vector4d& x, Eigen::VectorXd& dxdt, double t)
+{
+    // Name states to make it easier
+    double x_dot     = x(1);
+    double theta     = x(2);
+    double theta_dot = x(3);
+    double sin_theta = sin(theta);
+    double cos_theta = cos(theta);
+    double ctrl = _u(0);
+
+    // Set up dx
+    Eigen::VectorXd dx(4);
+    dx.setZero();
+
+    // Apply dynamics to calculate dx
+    dx(0) =  x_dot;
+    dx(1) =  (-bob_mass*pole_length*pow(theta_dot,2)*sin_theta + bob_mass*gravity*sin_theta*cos_theta + ctrl)/(cart_mass+bob_mass*pow(sin_theta,2));
+    dx(2) =  theta_dot;
+    dx(3) =  (-bob_mass*pole_length*pow(theta_dot,2)*sin_theta*cos_theta + (cart_mass+bob_mass)*gravity*sin_theta + ctrl*cos_theta) / (cart_mass*pole_length + bob_mass*pole_length*pow(sin_theta,2));
+    dxdt=dx;
+}
+
+
+void updateState()
+{
+    // Find timestep
+    double dt = (ros::Time::now() - prev_time).toSec();
+
+    // Set the placeholder state
+    Eigen::Vector4d x = state;
+
+    // Calculate the current state of the system using an ODE solver
+    // Integrate from time 0 every time because we only want to forward propagate one step
+    stepper.do_step(dynamics, x, 0, dt);
+
+    // Update state
+    state = x;
+
+    // Update the time tracking
+    prev_time = ros::Time::now();
+}
+
+
+void startCallback(const std_msgs::Empty::ConstPtr& empty)
+{
+    sim_started = true;
+    std::cout << "Cart-Pole Simulation started!\n";
+}
+
+void stopCallback(const std_msgs::Empty::ConstPtr& empty)
+{
+    sim_started = false;
+    std::cout << "Cart-Pole Simulation paused!\n";
+}
+
+
+void controlCallback(const gtddp_drone_msgs::ctrl_data::ConstPtr& ctrl)
 {
     // Update the current force
-    force = ctrl->data;
+    double force = ctrl->ctrl[0];
 
     // Make sure the force stays within the boundaries
     if(force < -max_force)
@@ -42,6 +106,10 @@ void controlCallback(const std_msgs::Float64::ConstPtr& ctrl)
     {
         force = max_force;
     }
+
+    // Update the dynamics given the control
+    _u(0) = force;
+    updateState();
 }
 
 
@@ -50,53 +118,37 @@ void statePubCallback(const ros::TimerEvent event)
     // Make the state data message
     gtddp_drone_msgs::state_data state_msg;
 
-    // Set the state data message to the right size
-    state_msg.state.resize(4);
-
-    // Populate the state
-    for(int i = 0; i < 4; ++i)
+    // Only publish while the simulation is active
+    if(sim_started)
     {
-        state_msg.state[i] = state(i);
-    }
+        // Set the state data message to the right size
+        state_msg.state.resize(4);
 
-    // Publish
-    state_pub.publish(state_msg);
+        // Populate the state
+        for(int i = 0; i < 4; ++i)
+        {
+            state_msg.state[i] = state(i);
+        }
+
+        // Publish
+        state_pub.publish(state_msg);
+    }
 }
 
 
 void simLoop(const ros::TimerEvent& event)
 {
     // Make the timer run once before calculating dt
-    if(!sim_init)
+    // Also don't run the simulator unless the start command has been sent
+    if(!sim_init || !sim_started)
     {
         sim_init = true;
+        prev_time = ros::Time::now();
         return;
     }
 
-    // Find timestep
-    double dt = (event.current_real - event.last_real).toSec();
-
-    // Name states to make it easier
-    double x_dot     = state(1);
-    double theta     = state(2);
-    double theta_dot = state(3);
-    double sin_theta = sin(theta);
-    double cos_theta = cos(theta);
-
-    // Apply dynamics to cart position and pole rotation
-    state(0) += x_dot * dt;
-    state(2) += theta_dot * dt;
-
-    // Constrain the pole rotation measurement to [0, 2*pi]
-    // state(2) = fmod(state(2), 2*M_PI);
-
-    // Apply control to the cart velocity
-    double cart_accel = (-bob_mass*pole_length*pow(theta_dot,2)*sin_theta + bob_mass*gravity*sin_theta*cos_theta + force)/(cart_mass+bob_mass*pow(sin_theta,2));
-    state(1) += cart_accel * dt;
-
-    // Apply control to the pole's angular velocity
-    double pole_velocity = (-bob_mass*pole_length*pow(theta_dot,2)*sin_theta*cos_theta + (cart_mass+bob_mass)*gravity*sin_theta + force*cos_theta) / (cart_mass*pole_length + bob_mass*pole_length*pow(sin_theta,2));
-    state(3) += pole_velocity * dt;
+    // Update the simulation state consistently
+    // updateState();
 }
 
 
@@ -133,18 +185,22 @@ int main(int argc, char **argv)
     max_force = cpsim.param<double>("/max_force", 100.0);
     pole_length = cpsim.param<double>("/length", 1.0);
 
-    // Initialize force
-    force = 0.0;
+    std::cout << bob_mass << " " << cart_mass << " " << max_force << " " << pole_length << std::endl;
 
     // Subscribe to the control signal
-    ros::Subscriber ctrl_sub = cpsim.subscribe(cpsim.resolveName("/control"), 1, &controlCallback);
+    ros::Subscriber ctrl_sub = cpsim.subscribe(cpsim.resolveName("/cart/control"), 1, &controlCallback);
+    ros::Subscriber start_sub = cpsim.subscribe(cpsim.resolveName("/cart/start"), 1, &startCallback);
+    ros::Subscriber stop_sub = cpsim.subscribe(cpsim.resolveName("/cart/stop"), 1, &stopCallback);
 
     // Publisher for the system state
     state_pub = cpsim.advertise<gtddp_drone_msgs::state_data>(cpsim.resolveName("/cart/state"), 1);
 
+    // Initialize the time
+    prev_time = ros::Time::now();
+
     // Simulator main thread and state estimator are run on timers
     ros::Timer sim_timer = cpsim.createTimer(ros::Duration(0.001), &simLoop);   // 1000 Hz
-    ros::Timer state_pub_timer = cpsim.createTimer(ros::Duration(1), &statePubCallback); // 100 Hz
+    ros::Timer state_pub_timer = cpsim.createTimer(ros::Duration(0.01), &statePubCallback); // 100 Hz
 
     // Keep thread alive / pump callbacks
     ros::spin();
